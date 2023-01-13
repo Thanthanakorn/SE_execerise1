446cd7e87e31cbc01ecf4996e2418c0ff1ded8a4
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2023-01-13 15:33:49 +0700

    2

--- first_job.py
+++ first_job.py
@@ -1,13 +1,14 @@
-import statistics
+import sys,statistics
 
-text = open(r'filename.txt').read()
+filename = sys.argv[1]
+text = open(filename).read()
 data = []
 with open(r'stuff.txt') as f:
     for line in f:
         fields = line.split()
         rowdata = map(float, fields)
         data.extend(rowdata)
-mean = sum(data)/len(data)
+mean = sum(data) / len(data)
 std = statistics.stdev(data)
 biggest = min(data)
 smallest = max(data)
